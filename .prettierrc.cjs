e585de99b176da6eaf852f5b51f17d69f1227a4b
screfy <screfy@pm.me>
2023-05-09 12:42:10 +0200

    Initial commit

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,17 +1,11 @@
-/**
- * @type {import('prettier').Options}
- */
+/** @type {import('prettier').Options} */
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: false,
-  trailingComma: "none",
-  bracketSpacing: true,
-  bracketSameLine: true,
-  plugins: [require.resolve("@plasmohq/prettier-plugin-sort-imports")],
-  importOrder: ["^@plasmohq/(.*)$", "^~(.*)$", "^[./]"],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true
-}
+	singleQuote: true,
+	plugins: [
+		require.resolve('@plasmohq/prettier-plugin-sort-imports'),
+		require.resolve('prettier-plugin-tailwindcss'),
+	],
+	importOrder: ['^@plasmohq/(.*)$', '^~(.*)$', '^[./]'],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+};
--- src/newtab.tsx
+++ src/newtab.tsx
@@ -1,25 +1,5 @@
-import { useState } from "react"
+import './styles.css';
 
-import "./style.css"
-
-function IndexNewtab() {
-  const [data, setData] = useState("")
-
-  return (
-    <div
-      className="new-tab"
-      style={{
-        padding: 16,
-        display: "flex",
-        flexDirection: "column"
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <footer>Crafted by @PlamoHQ</footer>
-    </div>
-  )
+export default function Newtab() {
+	return <h1 className="text-2xl font-bold">Hello world!</h1>;
 }
-
-export default IndexNewtab
