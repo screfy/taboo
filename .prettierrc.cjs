2b7c003d1f4719ab3c3a86f0564f70fcfc7f83f2
screfy <screfy@pm.me>
2024-03-25 15:59:30 +0100

    Bump deps and update to Node v20

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,11 +1,6 @@
 /** @type {import('prettier').Options} */
 module.exports = {
 	singleQuote: true,
-	plugins: [
-		require.resolve('@plasmohq/prettier-plugin-sort-imports'),
-		require.resolve('prettier-plugin-tailwindcss'),
-	],
-	importOrder: ['^@plasmohq/(.*)$', '^~(.*)$', '^[./]'],
-	importOrderSeparation: true,
-	importOrderSortSpecifiers: true,
+	trailingComma: 'es5',
+	plugins: [require.resolve('prettier-plugin-tailwindcss')],
 };
